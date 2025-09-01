3a9fe47f95dfc98572d993e3c2526c19c73dde25
Eva254-ke <evanszachariah36@gmail.com>
2025-09-01 11:10:17 +0300

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,15 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <head>
-              <meta charSet="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>Zachariah Evans</title>
-              <link rel="icon" href="/favicon.ico" />
-            </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
